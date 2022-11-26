a3e6760226321d37c0a64e1cbc42e57cdf0712fc
twang72 <71084257+twang72@users.noreply.github.com>
2022-11-26 11:35:56 -0800

    create the react application and set up the header of the homepage.

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Christmas Store</a>
+      </header>
+      <main>
+        List of Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
