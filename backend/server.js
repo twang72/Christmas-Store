6d9dadc35f69933850f190bfc4a17fe359c70437
codeeverydayyu <549117762@qq.com>
2022-11-27 19:48:14 -0800

    Connect to MongoDB in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); //fetch variables in .env file.
+//connect to mongodb
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 
