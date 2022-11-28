da18fb7be3d6b2e16ec626db33d311ea5a0581bb
codeeverydayyu <549117762@qq.com>
2022-11-28 00:07:07 -0800

    Create signin API, define generateToken, create new route to signin.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config(); //fetch variables in .env file.
 //connect to mongodb
@@ -17,8 +18,18 @@ mongoose
   });
 
 const app = express();
+//the form data in post request will be converted into a json object inside react.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter); //when user enter /api/seed, the seedRouter will response to it.
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+//when expressAsyncHandler catch an error, this will run and the error message will be returned.
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
