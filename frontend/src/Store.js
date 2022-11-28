03b6cb6fc7e6bbc6977998f18fe36bdef5114958
codeeverydayyu <549117762@qq.com>
2022-11-27 17:16:52 -0800

    Complete cartscreen. Save cart items to local storage,  make plus/minus/trash icon onClickHandler function on CartScreen, Add to cart/disable function on homescree, checkout button to redirect.

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -4,7 +4,9 @@ export const Store = createContext();
 
 const initialState = {
   cart: {
-    cartItems: [],
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [], //cartItems come from local storage, otherwise it's empty.
   },
 };
 
@@ -20,8 +22,15 @@ function reducer(state, action) {
             item._id === existItem._id ? newItem : item
           )
         : [...state.cart.cartItems, newItem];
+      localStorage.setItem('cartItems', JSON.stringify(cartItems)); //convert cartItems to string and save it to key 'cartItems' in local storage.
       return { ...state, cart: { ...state.cart, cartItems } };
-
+    case 'CART_REMOVE_ITEM': {
+      const cartItems = state.cart.cartItems.filter(
+        (item) => item._id !== action.payload._id
+      ); //if the item id doesn't equal to current id, return it; otherwise remove it.
+      localStorage.setItem('cartItems', JSON.stringify(cartItems));
+      return { ...state, cart: { ...state.cart, cartItems } };
+    } //use a {} here to not to mix with previous case's cartItems
     default:
       return state;
   }
