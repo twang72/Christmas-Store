230165131a738e0f86420b0c1606a2c853dc27fb
twang72 <71084257+twang72@users.noreply.github.com>
2022-11-30 15:12:42 -0800

    Recreate the get function for the order summary page.

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -38,7 +38,7 @@ export default function PlaceOrderScreen() {
   cart.itemsPrice = round2(
     cart.cartItems.reduce((a, c) => a + c.quantity * c.price, 0)
   );
-  cart.shippingPrice = cart.itemsPrice >= 30 ? round2(0) : round2(10);
+  cart.shippingPrice = cart.itemsPrice >= 50 ? round2(0) : round2(10);
   cart.taxPrice = round2(0.12 * cart.itemsPrice);
   cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
 
