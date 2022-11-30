bf887be0dd075124e1076df95c1b58eba3f9c5fa
twang72 <71084257+twang72@users.noreply.github.com>
2022-11-30 15:45:50 -0800

    Update the server.js for deploying.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
+import path from 'path';
 
 dotenv.config(); //fetch variables in .env file.
 //connect to mongodb
@@ -27,6 +27,12 @@ app.use('/api/seed', seedRouter); //when user enter /api/seed, the seedRouter wi
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 //when expressAsyncHandler catch an error, this will run and the error message will be returned.
 app.use((err, req, res, next) => {
